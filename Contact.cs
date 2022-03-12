ed5ecea1192dfdc85ebff0194805dab859b21857
rupalisam1999 <rupalisangale1999@gmail.com>
2022-03-12 22:02:44 +0530

    [Rupali]Refactpr: ADD multiple adressbook in asystem

--- Contact.cs
+++ Contact.cs
@@ -9,10 +9,25 @@ namespace AddressBook
     class Contact
     {
 
+        public Contact(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
         public string firstName { get; set; }
         public  string lastName { get; set; }
         public string address { get; set; }
-        public string phoneNo { get; set; }
+        public string phoneNumber { get; set; }
+
+        public string city { get; set; }
+
         public string email { get; set; }
         public string state { get; set; }
         public string zip { get; set; }
